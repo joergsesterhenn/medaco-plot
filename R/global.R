eda828836824ec1eb23d54a1e1afacffe06a3741
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2025-03-09 16:25:45 +0100

    use r4.4.3, fix import

--- R/global.R
+++ R/global.R
@@ -1,5 +1,5 @@
-library(here)
-here::i_am("Readme.md")
+#' @import here
+here::i_am("R/global.R")
 
 i18n <- shiny.i18n::Translator$new(
   translation_json_path = here::here("inst", "translations.json")
